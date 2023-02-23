94771675f357ec4b559eb1a9141d506023195084
phoudail <philemon.houdaille@ens-rennes.fr>
2023-02-23 11:01:50 +0100

    Added proper indent to TreePrinter

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,11 @@
-use polyglot_ast::polyglot_tree::{PolyglotTree, polyglot_zipper};
+use polyglot_ast::polyglot_tree::PolyglotTree;
 
 fn main() {
-    // use tree_sitter::Parser;
-    //     let code = r#"
-    //     function double(x) {
-    //         return x * 2;
-    //     }
-    // "#;
-    //     let mut parser = Parser::new();
-    //     parser
-    //         .set_language(tree_sitter_javascript::language())
-    //         .expect("Error loading JavaScript grammar");
-    //     let parsed = parser.parse(code, None);
-    //     println!("{:#?}", parsed);
-
-    //     let code = r#"
-    //         def double(x):
-    //             return x * 2
-    //             "#;
-    //     let mut parser = Parser::new();
-    //     parser
-    //         .set_language(tree_sitter_python::language())
-    //         .expect("Error loading Python grammar");
-    //     let parsed = parser.parse(code, None);
-    //     println!("{:#?}", parsed);
     let tree = PolyglotTree::from(
         "x = 42\npolyglot.eval(path=\"GraalSamples/test_b.py\", language=\"python\")",
         polyglot_ast::util::Language::Python,
-    ).expect("Should not have parsing issues");
+    )
+    .expect("Should not have parsing issues");
     let mut tp = polyglot_ast::polyglot_tree::polyglot_processor::TreePrinter::new();
     tree.apply(&mut tp);
     println!("{}", tp.get_result())
--- src/polyglot_tree/polyglot_processor.rs
+++ src/polyglot_tree/polyglot_processor.rs
@@ -17,26 +17,39 @@ impl TreePrinter {
         }
     }
 
+    pub fn from(&self) -> TreePrinter{
+        TreePrinter {
+            indent_level: self.indent_level,
+            result: String::new(),
+        }
+    }
+
     pub fn get_result(&self) -> &str {
         self.result.as_str()
     }
 }
 
 impl PolygotProcessor for TreePrinter {
-    fn process(&mut self, zip: PolyglotZipper) {
-        let mut res = String::from(" ").repeat(self.indent_level);
-        res.push_str(&format!("{}\n", zip.kind()));
-        self.result.push_str(&res);
+    fn process(&mut self, zip: PolyglotZipper) { // TODO fix indent
+        let mut indent = String::from(" ").repeat(self.indent_level);
 
         let child = zip.child(0);
         match child {
-            Some(z) => self.process(z),
-            None => (),
+            Some(z) => {
+                indent.push_str(&format!("{}\n", zip.kind()));
+                self.result.push_str(&indent);
+                self.indent_level += 1;
+                self.process(z);
+            }
+            None => {
+                indent.push_str(&format!("{} : {}\n", zip.kind(), zip.code()));
+                self.result.push_str(&indent);
+            }
         };
 
         let sibling = zip.next_sibling();
         match sibling {
-            Some(z) => self.process(z),
+            Some(z) => {let mut nextp = self.from(); nextp.process(z); self.result.push_str(nextp.get_result()) },
             None => (),
         }
     }
